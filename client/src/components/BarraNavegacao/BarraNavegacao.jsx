d333714292853a6fd0a7849a491b277a36ddc515
Gregory Veloso <gregory.v.silva@aluno.senai.br>
2025-10-21 19:20:26 -0300

    Sim

---
+++ client/src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -0,0 +1,15 @@
+// Importando o css da barra de navegação
+import styles from "./BarraNavegacao.module.css"
+
+// Importar os componentes do bootstrap
+import { Nav, Navbar, NavDropdown, Image, Accordion } from "react-bootstrap"
+
+const BarraNavegacao = () => {
+  return (
+    <div>
+        BarraNavegacao
+    </div>
+  )
+}
+
+export default BarraNavegacao
